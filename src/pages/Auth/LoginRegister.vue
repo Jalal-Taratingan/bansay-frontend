1df8683dbc0a5ce158b2a228d38cbbfcd176338b
fencim <fencim@gmail.com>
2025-11-18 08:59:01 +0800

    route admin user to dashboard

--- src/pages/Auth/LoginRegister.vue
+++ src/pages/Auth/LoginRegister.vue
@@ -118,6 +118,7 @@ import logo from 'src/assets/logo.png';
 import 'boxicons/css/boxicons.min.css';
 import { useAuthStore } from 'src/stores/auth-store';
 import { type UserRegisterDtoRoleEnum } from 'src/services/sdk';
+import { useRouter } from 'vue-router';
 const loginUsername = ref('');
 const loginPassword = ref('');
 
@@ -144,11 +145,17 @@ watch([password, confirmPassword], () => {
     passwordError.value = '';
   }
 });
+const $router = useRouter();
 async function login() {
-  await authStore.login({
+  const response = await authStore.login({
     username: loginUsername.value,
     password: loginPassword.value,
   });
+  if (/^admin$/i.test(response.user?.role)) {
+    await $router.replace({
+      name: 'admin-dashboard',
+    });
+  }
 }
 
 async function register() {
